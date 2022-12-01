4951d795bd46a075a3c2b901353b2fbb76edf052
Samuel Johnston <S.Johnston1@newcastle.ac.uk>
2022-12-01 14:46:01 +0000

    FPS + Memory count

--- Assets/UI.cs
+++ Assets/UI.cs
@@ -1,9 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
+    [SerializeField]
+    Text fpsText;
+
+    [SerializeField]
+    Text memoryText;
+
+    [SerializeField]
+    float updateInterval = 0.05f; // update every 0.05 seconds
+
+    float currentTime = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +25,21 @@ public class UI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(1/Time.unscaledDeltaTime);
+        currentTime += Time.unscaledDeltaTime;
+
+        if (currentTime > updateInterval)
+        {
+            currentTime = 0;
+
+            float fps = 0;
+            if (Time.unscaledDeltaTime != 0)
+                fps = 1 / Time.unscaledDeltaTime;
+
+            float memory = UnityEngine.Profiling.Profiler.GetTotalAllocatedMemoryLong() / 1000000; // get memory usage and convert to megabytes
+
+            fpsText.text = (int)fps + " FPS";
+            memoryText.text = "Memory: " + memory + " MB";
+        }
+
     }
 }
